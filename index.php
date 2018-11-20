2e4a1e697afae849a5391e5a827525d0c1035d07
adi <adisianturi02@gmail.com>
2018-11-20 19:38:07 +0700

    Update index.php

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>WEBSITE JUAL SEPATU APALAH</title>
+</head><link rel="stylesheet" type="text/css" href="style.css">
+<body>
+<div class="nama">
+	ROEMAH SEPATU
+</div>
+	
+<div id="header">
+
+<ul>
+
+ <li><a href="##">HOME</a></li>
+ <li><a href="##">PRODUK</a></li>
+ <li><a href="##">CONTACT</a></li>
+ <li><a href="##">ABOUT</a></li> 
+</ul>
+</div>
+	
+	<div class="clear"></div>
+</div>
+<div id="slider">
+	<figure>
+
+		<img src="pict/sepatu14ab.jpg">
+		<img src="pict/sepatu18a.jpg">
+		<img src="pict/sepatu16.jpg">
+		<img src="pict/sepatu17a.jpg">
+	</figure>
+</div>
+
+
+<div class="mengapa">
+	<p>ROEMAH SEPATU</p><br>
+ INGIN GAYA? ROEMAH SEPATU SOLUSINYA <br>
+	
+</div>
+<div class="layanan">
+	<div><img src="pict/hargaa.png"><br>
+	<p>Harga Terjangkau </p><br>
+Harga termurah sesuai dengan kulaitas</div>
+<div><img src="pict/kualitas.png"><br>
+	<p>Kualitas Terbaik </p><br>
+Kualitas yang diberikan adalah yang terbaik</div>
+<div><img src="pict/uptodate.png"><br>
+	<p>produk Yang Up To date </p><br>
+Kami selalu memberikan produk produk yang terbaru</div>
+<div><img src="pict/cc.png"><br>
+	<p>Customer Care </p><br>
+Selalu siap menerima Keluhan dan Kebutuhan Informasi dari Pelanggan</div>
+</div>
+
+
+<div class="bawah">
+	 
+		
+	<div class="line"> </div>
+</div>
+
+<div id="footer3">
+<b>KUNJUNGI KANTOR KAMI<br>
+	Jln Sisingamangaraja Medan <br>
+Medan, Sumatera Utara<br>
+Telp. (061) 4537346<br>
+Fax. (061) 4537342<br>
+E-mail. roemah.sepatu@gmail.com<br></b>
+	
+</div>
+
+</div><div id="footer">
+<marquee >@copyright 2018</marquee>
+
+</div>
+
+
+</body>
+</html>
